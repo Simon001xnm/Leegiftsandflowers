f1db747dd2cc2d7d9c928f012d3963951c433e54
SIMON STYLES Ö <wanjirumaina539@gmail.com>
2026-07-21 23:27:48 +0000

    Try fixing this error: `Runtime ReferenceError: Separator is not defined

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { 
   LayoutDashboard, 
   Search, 
